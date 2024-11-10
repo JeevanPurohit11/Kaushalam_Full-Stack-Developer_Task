cf3d9677429fa1abd358e0c3f7fb9689409b1dae
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-11-10 15:39:32 +0530

    frontend Versal config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://kaushalam-full-stack-developer-task.vercel.app/',
         changeOrigin: true,
         secure: false,
       },
